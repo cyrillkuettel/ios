80147e2c9263cc366a2479e88ae8f8551f832948
cyrillkuettel <cyrillkuettel@gmail.com>
2022-10-09 21:00:44 +0200

    Add models

---
+++ sw02/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by HSLU-N0004890 on 09.10.22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
