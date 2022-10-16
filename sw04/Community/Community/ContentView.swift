91f62324bcb9d796309a148262725edbd57cd1fb
cyrillkuettel <cyrillkuettel@gmail.com>
2022-10-16 17:24:49 +0200

    Refactoring: move stuff to Community class

--- sw04/Community/Community/ContentView.swift
+++ sw04/Community/Community/ContentView.swift
@@ -3,90 +3,43 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let community = Community()
-    @State var shuffleCount: Int = 0
-    // writeonce here todo:
+    @StateObject var community = Community()
     
-    let Many = 1_000_000
-    let Few = 1000
+
     var body: some View {
         
         NavigationView {
             List {
-                Button(action: { self.shuffleFew() }) {
+                Button(action: { self.community.shuffleFew() }) {
                     Text("Shuffle Few")
                 }
-                Button(action: { self.synchronouslyShuffleMany() })  {
+                Button(action: { self.community.synchronouslyShuffleMany() })  {
                     Text("Schuffle Many Synchronized")
                 }
-                Button(action: { self.shuffleConcurrent() })  {
-                    Text("Shuffle Concurrent")
+                Button(action: { self.community.shuffleConcurrent() })  {
+                    Text("Shuffle Concurrent (Thread)")
                 }
                 
                 Button("Shuffle Twice") {
                     
                     Task(priority: .high) {
-                        await self.shuffleConcurrentAsync()
+                        await self.community.shuffleConcurrentAsync()
                     }
                     Task(priority: .low) {
-                        await self.shuffleConcurrentAsync()
+                        await self.community.shuffleConcurrentAsync()
                     }
                 }
-                
                 
                 Text("Initial Total: 50000")
                 Text("10000 | 10000 | 10000 | 10000 | 10000")
                 Text("Last Total: 50000")
-                Text("\(shuffleCount) times shuffled")
+                Text("\(community.shuffleCount) times shuffled")
             }
             .navigationBarTitle(Text("Shuffle"))
         }
     }
     
-    
-    
-    private func shuffleConcurrentAsync() async {
-    
-            for _ in 0...Many {
-                synchronouslyShuffleMany()
-            }
-        
-    }
 
-    
-    private func shuffleFew() {
-        for _ in 0..<Few {
-            if let lender = self.community.members.randomElement(){
-                if let borrower = self.community.members.randomElement() {
-                    lender.lend(borrower: borrower)
-                } else {
-                    print("random Element is nil wtf")
-                }
-            } else {
-                print("random Element is nil wtf")
-            }
-            self.shuffleCount += 1
-        }
-    }
-   
-    private func synchronouslyShuffleMany() {
-        for _ in 0..<Many {
-            if let lender = self.community.members.randomElement(){
-                if let borrower = self.community.members.randomElement() {
-                    lender.lend(borrower: borrower)
-                }
-            }
-            self.shuffleCount += 1
-        }
-    }
-    
-    private func shuffleConcurrent() {
-        let thread = Thread {
-            for _ in 0...Many {
-                synchronouslyShuffleMany()
-            }
-        }
-    }
 
 }
 
--- sw04/Community/Community/Models/Community.swift
+++ sw04/Community/Community/Models/Community.swift
@@ -1,9 +1,53 @@
 
 import Foundation
 
-class Community {
+class Community : ObservableObject{
 
 //  let members: [Member] = Array(repeating:  Member(), count: 5)
     
     let members: [Member] = [Member(), Member(), Member(), Member(), Member()]
+    let Many = 1_000_000
+    let Few = 1000
+    
+    @Published var shuffleCount: Int = 0
+
+    public func shuffleConcurrentAsync() async {
+        for _ in 0...Many {
+            synchronouslyShuffleMany()
+        }
+    }
+
+    
+    public func shuffleFew() {
+        for _ in 0..<self.Few {
+            if let lender = self.members.randomElement(){
+                if let borrower = self.members.randomElement() {
+                    lender.lend(borrower: borrower)
+                } else {
+                    print("random Element is nil wtf")
+                }
+            } else {
+                print("random Element is nil wtf")
+            }
+            self.shuffleCount += 1
+        }
+    }
+   
+    public func shuffleConcurrent() {
+        let thread = Thread {
+            for _ in 0...self.Many {
+                self.synchronouslyShuffleMany()
+            }
+        }
+    }
+    public func synchronouslyShuffleMany() {
+        for _ in 0..<self.Many {
+            if let lender = self.members.randomElement(){
+                if let borrower = self.members.randomElement() {
+                    lender.lend(borrower: borrower)
+                }
+            }
+            self.shuffleCount += 1
+        }
+    }
 }
