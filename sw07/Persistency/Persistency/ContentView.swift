ff0b21a808b13741353c9ca59758a68984e321a1
cyrillkuettel <cyrillkuettel@gmail.com>
2022-11-01 15:25:32 +0100

    Clean up code

--- sw07/Persistency/Persistency/ContentView.swift
+++ sw07/Persistency/Persistency/ContentView.swift
@@ -14,12 +14,12 @@ struct ContentView: View {
             List {
                 ForEach(persons) { person in
                     NavigationLink {
-                        Text("Item at \(person.name!)")
+                        Text("Person with name \(person.name!)")
                     } label: {
                         Text(person.name!)
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .onDelete(perform: deletePersons)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
@@ -31,7 +31,7 @@ struct ContentView: View {
                     }
                 }
             }
-            Text("Select an item")
+            Text("Select an Person")
         }
     }
 
@@ -43,23 +43,19 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
 
-    private func deleteItems(offsets: IndexSet) {
+    private func deletePersons(offsets: IndexSet) {
         withAnimation {
             offsets.map { persons[$0] }.forEach(viewContext.delete)
 
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -67,13 +63,6 @@ struct ContentView: View {
     }
 }
 
-// not used currently
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
