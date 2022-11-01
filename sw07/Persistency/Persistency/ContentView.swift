8fc82caf69db37ed745b8b1011c80b0995a871cc
cyrillkuettel <cyrillkuettel@gmail.com>
2022-11-01 16:47:14 +0100

    created two lists, to-Many relationship

--- sw07/Persistency/Persistency/ContentView.swift
+++ sw07/Persistency/Persistency/ContentView.swift
@@ -2,15 +2,40 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    
     @Environment(\.managedObjectContext) private var viewContext
-
+    
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Person.name, ascending: true)],
         animation: .default)
     private var persons: FetchedResults<Person>
+    
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Team.id, ascending: true)],
+        animation: .default)
+    private var teams: FetchedResults<Team>
+    
+    let persistentContainer = PersistenceController.shared.container
 
+    
+    let someNames = ["Frodo", "Samwise", "Merry", "Pippin"]
+    
+    let someTeams =
+    ["HomeRunnerz",
+     "Four Baggers",
+     "SlumpBuster",
+     "Hit Machines",
+     "Little League Wannabe",
+     "Evil Empire",
+     "Softball Jones",
+     "Alley Cats",
+     "Street Gang",
+     "The Sandlotters"]
+    
     var body: some View {
         NavigationView {
+            
             List {
                 ForEach(persons) { person in
                     NavigationLink {
@@ -21,25 +46,93 @@ struct ContentView: View {
                 }
                 .onDelete(perform: deletePersons)
             }
+    
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
-                ToolbarItem {
-                    Button(action: addPerson) {
-                        Label("Add Item", systemImage: "plus")
+            }
+            .navigationBarTitle("Persons", displayMode: .inline) 
+        }
+        NavigationView {
+            List {
+                ForEach(teams) { team in
+                    NavigationLink {
+                        Text("Team with name \(team.id!)")
+                    } label: {
+                        Text(team.name!)
                     }
                 }
+                .onDelete(perform: deleteTeams)
             }
-            Text("Select an Person")
+
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    EditButton()
+                }
+            }
+            .navigationBarTitle("Teams", displayMode: .inline) // ⬅️ Important part
+        }
+        VStack{
+            Button(action: addPerson){
+                Text("New Person")
+                    .cornerRadius(5)
+                    .padding(10)
+                    .buttonBorderShape(.roundedRectangle)
+
+            }
+            Button(action: createTeamWith2Person){
+                Text("New Team with two Persons")
+                    .cornerRadius(5)
+                    .padding(10)
+                    .buttonBorderShape(.roundedRectangle)
+            }
+            Button(action: {}){
+                Text("New Person with two Teams")
+                    .cornerRadius(5)
+                    .padding(10)
+                    .buttonBorderShape(.roundedRectangle)
+            }
+            Button(action: {}){
+                Text("Clear All")
+                    .cornerRadius(5)
+                    .padding(10)
+                    .buttonBorderShape(.roundedRectangle)
+            }
         }
     }
+    
+    private func createTeamWith2Person() {
+            withAnimation {
+                let newTeam = Team(context: viewContext)
+                newTeam.id = UUID()
+                newTeam.points = Int64.random(in: 0..<11)
+                newTeam.name = someTeams.randomElement()
+                
+                let newPerson1 = Person(context: viewContext)
+                newPerson1.name = "Foo"
+                let newPerson2 = Person(context: viewContext)
+                newPerson2.name = "Bar"
+                
+                newTeam.addToRelationship(newPerson1)
+                newTeam.addToRelationship(newPerson2)
 
+                do {
+                    try viewContext.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
+            }
+        }
+    
     private func addPerson() {
         withAnimation {
-            let newItem = Person(context: viewContext)
-            newItem.name = "bar"
-
+            let newPerson = Person(context: viewContext)
+            newPerson.name = someNames.randomElement()
+            
             do {
                 try viewContext.save()
             } catch {
@@ -48,11 +141,11 @@ struct ContentView: View {
             }
         }
     }
-
+    
     private func deletePersons(offsets: IndexSet) {
         withAnimation {
             offsets.map { persons[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
@@ -61,6 +154,40 @@ struct ContentView: View {
             }
         }
     }
+    
+    
+    private func addTeam() {
+        withAnimation {
+            let newTeam = Team(context: viewContext)
+            newTeam.name = someTeams.randomElement()
+            newTeam.id = UUID()
+            newTeam.points = Int64.random(in: 0..<11)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    private func deleteTeams(offsets: IndexSet) {
+        withAnimation {
+            offsets.map { teams[$0] }.forEach(viewContext.delete)
+            
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+    
+
+    
+
 }
 
 
