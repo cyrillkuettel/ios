ee093ff008c1ef7f3819149d221d1b69bc42ded4
cyrillkuettel <cyrillkuettel@gmail.com>
2022-11-20 15:10:37 +0100

    minor changes

--- sw07/Persistency/Persistency/CodableFileWrite.swift
+++ sw07/Persistency/Persistency/CodableFileWrite.swift
@@ -1,42 +1,38 @@
 import Foundation
 
 class CodableFileWrite<T: Codable> {
-
-
+    
+    
     static func write(_ content: T, to fileName: String) throws -> Bool {
-
+        
         
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory,
-                                                              in: .userDomainMask).first!
-        let fileURL = documentsDirectory.appendingPathComponent(fileName).appendingPathExtension("txt")
+                                                          in: .userDomainMask).first!
         
-        if fileExists(path: fileURL.path) {
-            fatalError("File does already exist.")
-        } else {
-            let data: Data = try! JSONEncoder().encode(content)
-            let success = FileManager.default.createFile(atPath: fileURL.absoluteString, contents: data)
-            return success
-        }
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        
+        let data: Data = try! JSONEncoder().encode(content)
+        let success = FileManager.default.createFile(atPath: fileURL.absoluteString, contents: data)
+        return success
     }
     
     
     static func read(from fileName: String) throws -> T {
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory,
-                                                              in: .userDomainMask).first!
-        let fileURL = documentsDirectory.appendingPathComponent(fileName).appendingPathExtension("txt")
-
+                                                          in: .userDomainMask).first!
+        let fileURL = documentsDirectory.appendingPathComponent(fileName)
+        
         let data = FileManager.default.contents(atPath: fileName)!
         let o = try! JSONDecoder().decode(T.self, from: data)
         return o
     }
-
     
     
-    static func fileExists(path: String) -> Bool {
-        var isDirectory: ObjCBool = false
-        let fm = FileManager.default
-        return (fm.fileExists(atPath: path, isDirectory: &isDirectory))
+    
+    static func doesFileExist(url: URL) -> Bool {
+        let fileManager = FileManager.default
+        return fileManager.fileExists(atPath: url.path)
     }
-   
+    
     
 }
--- sw07/Persistency/PersistencyTests/PersistencyTests.swift
+++ sw07/Persistency/PersistencyTests/PersistencyTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 @testable import Persistency
 
+
 final class PersistencyTests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -17,6 +18,29 @@ final class PersistencyTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+    
+    func testCodableFileWrite() {
+        
+        
+        struct Object : Codable, Equatable {
+            let value: String
+            
+            static func ==(lhs: Object, rhs: Object) -> Bool {
+                return lhs.value == rhs.value
+            }
+        }
+        
+        let writeInput = Object(value: "foo")
+       
+        let success = try! CodableFileWrite<Object>.write( writeInput, to: "someFile")
+        
+        XCTAssertTrue(success)
+        
+        let readOutput = try! CodableFileWrite<Object>.read(from: "someFile")
+        
+        XCTAssertEqual(readOutput, writeInput)
+        
     }
 
     func testPerformanceExample() throws {
